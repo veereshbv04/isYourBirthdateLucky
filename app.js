af37ffebe0d7003f9122c3231276a7beb8b363db
Veeresh B V <Veereshbv04@gmail.com>
2021-07-24 00:34:27 +0530

    event listener is working

--- app.js
+++ app.js
@@ -5,17 +5,31 @@ const outputDiv = document.querySelector(".output");
 
 const checkButton = document.querySelector("#check");
 
+const outputlucky = "<div><h2>Heeey You are a Lucky <h2><img width='100%' src='' /></div>"
+const outputnotlucky = "<div><h1>Sorry ,Your Birthdate is not a lucky number </h1></div>"
+
 checkButton.addEventListener("click",()=>{
-    alert(date.value);
+    
+
     const dateString = ((date.value).split('-')).join("");
     var sum=0;
     // converting string to letter
-
+    console.log("ss");
     for( ele in dateString){
         sum = sum + Number(ele);
     }
+
     const luckyno = Number(luckyNumber.value);
+
     // Checking if Date is divisible by Lucky Number
     
+    if (sum%luckyno === 0){
+        console.log("lucky");
+        return outputDiv.innerHTML = outputlucky;
+    }
+    else{
+            return outputDiv.innerHTML = outputlucky;
+            console.log("NOt Lucky");
+    }
     
 })
